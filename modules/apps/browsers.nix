e5d6939be9f8e8d8221922313caa3a9acb174ef8
Aleksandr Medvedev <a.medvedev@just-ai.com>
2026-05-04 23:19:14 +0300

    fix: stdenv.hostPlatform instead system

--- modules/apps/browsers.nix
+++ modules/apps/browsers.nix
@@ -3,7 +3,7 @@
 {
   environment.systemPackages = [
     # Zen Browser из стороннего flake
-    inputs.zen-browser.packages.${pkgs.system}.default
+    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
     # Chromium (опционально — для совместимости)
     pkgs.chromium
   ];
