60a1df4c93f5e4dc278b5d6c934bf5cfa870ba4f
Aleksandr Medvedev <a.medvedev@just-ai.com>
2026-05-05 13:38:11 +0300

    add mobile

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -11,13 +11,14 @@
     settings = {
       devices = {
         "laptop-intel" = { id = "NQLLZN3-557I4N6-5MYIHUM-QDG4NE5-SDZRUTW-PDMPM56-D7OVJ2B-6P4LSQB"; };
+        "mobile" = { id = "X5RCHHT-DBFMWN2-UXHNJEC-TBM6MV6-CSDKSLO-7V7KRGG-QIBLHEM-JKGXPAR"; };
         # "device2" = { id = "DEVICE-ID-GOES-HERE"; };
       };
 
       folders = {
         "Documents" = {
           path = "/home/alexmcgil/Documents";
-          devices = [ "laptop-intel" ];
+          devices = [ "laptop-intel" "mobile" ];
         };
         "Scripts" = {
           path = "/home/alexmcgil/scripts";
