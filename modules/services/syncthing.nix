8df17a93c70facfb4330336a5d6d1cb6236d8aa6
Aleksandr Medvedev <a.medvedev@just-ai.com>
2026-05-05 13:24:55 +0300

    add syncthing config

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -7,5 +7,30 @@
     dataDir = "/home/alexmcgil";
     configDir = "/home/alexmcgil/.config/syncthing";
     openDefaultPorts = true;
+
+    settings = {
+      devices = {
+        "laptop-intel" = { id = "NQLLZN3-557I4N6-5MYIHUM-QDG4NE5-SDZRUTW-PDMPM56-D7OVJ2B-6P4LSQB"; };
+        # "device2" = { id = "DEVICE-ID-GOES-HERE"; };
+      };
+
+      folders = {
+        "Documents" = {
+          path = "/home/alexmcgil/Documents";
+          devices = [ "laptop-intel" ];
+        };
+        "Scripts" = {
+          path = "/home/alexmcgil/scripts";
+          devices = [ "laptop-intel" ];
+          ignorePerms = false;
+        };
+        # "Example" = {
+        #   path = "/home/myusername/Example";
+        #   devices = [ "device1" ];
+        # };
+      };
+    };
   };
+
+  networking.firewall.allowedTCPPorts = [ 8384 ];
 }
