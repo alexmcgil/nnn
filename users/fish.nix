c15710b116ae86159d77d2801583c293d1333765
Aleksandr Medvedev <a.medvedev@just-ai.com>
2026-05-06 18:08:51 +0300

    clear fish

--- users/fish.nix
+++ users/fish.nix
@@ -9,25 +9,11 @@
       # No greeting
       set fish_greeting
 
-      # Use starship
-      function starship_transient_prompt_func
-          starship module character
-      end
-      if test "$TERM" != "linux"
-          starship init fish | source
-          enable_transience
-      end
-
-      # Colors from quickshell
-      if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-          cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-      end
-
       # Aliases
       alias clear "printf '\033[2J\033[3J\033[1;1H'"
       alias celar "printf '\033[2J\033[3J\033[1;1H'"
       alias claer "printf '\033[2J\033[3J\033[1;1H'"
-      alias q 'qs -c ii'
+
       if test "$TERM" != "linux"
           alias ls 'eza --icons'
       end
@@ -97,43 +83,4 @@
     };
   };
 
-  # ---- Fish conf.d ----
-  xdg.configFile."fish/conf.d/fish_frozen_key_bindings.fish".text = ''
-    # Migrated from fish 4.3 upgrade — erases universal variable
-    set --erase --universal fish_key_bindings
-  '';
-
-  xdg.configFile."fish/conf.d/fish_frozen_theme.fish".text = ''
-    set --global fish_color_autosuggestion 555 brblack
-    set --global fish_color_cancel -r
-    set --global fish_color_command blue
-    set --global fish_color_comment red
-    set --global fish_color_cwd green
-    set --global fish_color_cwd_root red
-    set --global fish_color_end green
-    set --global fish_color_error brred
-    set --global fish_color_escape brcyan
-    set --global fish_color_history_current --bold
-    set --global fish_color_host normal
-    set --global fish_color_host_remote yellow
-    set --global fish_color_normal normal
-    set --global fish_color_operator brcyan
-    set --global fish_color_param cyan
-    set --global fish_color_quote yellow
-    set --global fish_color_redirection cyan --bold
-    set --global fish_color_search_match --background=111
-    set --global fish_color_selection white --bold --background=brblack
-    set --global fish_color_status red
-    set --global fish_color_user brgreen
-    set --global fish_color_valid_path --underline
-    set --global fish_pager_color_completion normal
-    set --global fish_pager_color_description B3A06D yellow -i
-    set --global fish_pager_color_prefix cyan --bold --underline
-    set --global fish_pager_color_progress brwhite --background=cyan
-    set --global fish_pager_color_selected_background -r
-  '';
-
-  xdg.configFile."fish/conf.d/uv.env.fish".text = ''
-    source "$HOME/.local/bin/env.fish"
-  '';
 }
